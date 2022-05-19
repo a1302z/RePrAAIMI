3010d132b68eaf101a7bf1b3de4646e1e79ac291
Alex Ziller <alex.ziller@tum.de>
2022-05-19 16:25:23 +0200

    updated installation

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
-mamba env create -f environment.yml && conda activate objaxdp && echo $(which pip) && pip install "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases/jax_releases.html && pip install --upgrade objax
+conda env create -f environment.yml && conda activate objaxdp && echo $(which pip) && pip install --upgrade jax[cuda]==0.3.7 jaxlib==0.3.7+cuda11.cudnn82 -f https://storage.googleapis.com/jax-releases/jax_releases.html && pip install objax
+# --upgrade jax==0.3 jaxlib==0.3+cuda11.cudnn805 -f https://storage.googleapis.com/jax-releases/jax_releases.html
+# && pip install --upgrade "jax[cuda11_cudnn81]"
