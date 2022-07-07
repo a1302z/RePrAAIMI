8b5412797f9a9750514c4d17eeada349e62977a6
Alex Ziller <alex.ziller@tum.de>
2022-07-07 12:38:18 +0200

    two more test cases for ema + pylints

--- test/utils/test_ema.py
+++ test/utils/test_ema.py
@@ -1,4 +1,5 @@
 import numpy as np
+import objax
 from jax import numpy as jn
 import sys
 from pathlib import Path
@@ -25,3 +26,25 @@ def test_update_ema():
     ema.update(model.vars())
     ema.copy_to(model.vars())
     assert all([jn.all(jn.isclose(v, 0.9)).item() for v in model.vars().values()])
+
+
+def test_jit_ema():
+    decay = 0.01
+    model_vars = model.vars()
+    model_var_refs = {k: objax.TrainRef(v) for k, v in model_vars.items()}
+    for key, mv in model_var_refs.items():
+        model_var_refs[key].assign(jn.ones_like(mv))
+    ema = ExponentialMovingAverage(model.vars(), decay, use_num_updates=False)
+
+    @objax.Function.with_vars(model_vars)
+    def increase_model_weights():
+        for mv in model_vars.values():
+            ref = objax.TrainRef(mv)
+            ref.value += jn.ones_like(mv)
+
+    increase_model_weights = objax.Jit(increase_model_weights)
+
+    for _ in range(3):
+        increase_model_weights()
+        ema.update(model_vars)
+        ema.copy_to(model_vars)
