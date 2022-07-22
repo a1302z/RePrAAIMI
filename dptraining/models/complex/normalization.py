9c8d2ba2f64d0cade9bc39cb3cd190f059f518b7
Georgios Kaissis <48105485+gkaissis@users.noreply.github.com>
2022-07-22 19:39:25 +0200

    move normalization process outside vmap

--- dptraining/models/complex/normalization.py
+++ dptraining/models/complex/normalization.py
@@ -68,7 +68,7 @@ class ComplexGroupNorm2DWhitening(ComplexGroupNormWhitening):
         u = jn.stack((z.real, z.imag), 2).reshape(N, C, 2, -1)  # N, C, 2, ...
         # (1, C, 2, 2) @ (N, C, 2, ...) -> (N, C, 2, ...)
         # (N, C, 2, ...) + (1, C, 2, 1) -> (N, C, 2, ...)
-        r = jn.matmul(self.weight, u) + self.bias
+        r = jn.matmul(self.weight.value, u) + self.bias.value
         return (r[:, :, 0, :] + 1j * r[:, :, 1, :]).reshape(N, C, H, W)
 
     def __repr__(self) -> str:
@@ -78,37 +78,15 @@ class ComplexGroupNorm2DWhitening(ComplexGroupNormWhitening):
 # Batch Norm
 
 
-def bn_single(
-    vec: JaxArray,
-    mean: JaxArray | None = None,
-    sigma: JaxArray | None = None,
-) -> tuple[JaxArray, JaxArray, JaxArray]:
-    """Takes a complex vector with three dimensions and
-    1. computes the mean if it's not provided
-    2. computes the covariance matrix if it's not provided
-    3. centers and whitens the vector
-    4. returns the normalized vector and the running statistics
-
-    This method is optimised for batch norm where it is supposed to
-    be vmapped over the channel dimensions. For this, first move the
-    channel axis to be first. In training mode, leave mean and sigma
-    as None and use the return to update the running stats.
-    In inference mode, send in the running mean and covariance.
-    Then move the channel axis back to the original position.
-    """
-    split = jn.stack([vec.real, vec.imag]).reshape(2, -1)
-    if mean is None:
-        mean = split.mean(-1)
-    centered = split - mean.reshape(2, 1)
-    if sigma is None:
-        sigma = jn.cov(split[0], split[1])
+def compute_whitening_matrix_single(
+    sigma: JaxArray,
+) -> JaxArray:
     u_mat, lmbda, _ = jn.linalg.svd(sigma, full_matrices=False)
     w_mat = jn.dot(u_mat, jn.dot(jn.diag(1.0 / jn.sqrt(lmbda + 1e-5)), u_mat.T))
-    result = w_mat @ centered
-    return result, mean, sigma
+    return w_mat
 
 
-batch_normalize = vmap(bn_single)
+compute_whitening_matrix = vmap(compute_whitening_matrix_single)
 
 
 class ComplexBatchNorm2D(Module):
@@ -144,22 +122,28 @@ class ComplexBatchNorm2D(Module):
         self.weight = TrainVar(weight)  # (n_channels, 2, 2)
 
     def __call__(self, x: JaxArray, training: bool | None = True) -> JaxArray:
-        in_shape = x.shape
+        N, C, H, W = x.shape
         x = jn.swapaxes(x, 0, 1)  # channels first
+        split = jn.stack((x.real, x.imag)).reshape(C, 2, -1)
         if training:
-            y, mean, sigma = batch_normalize(x)
+            mean = split.mean(axis=-1)  # C, 2
+            sigma = (split @ split.transpose(0, 2, 1)) * (1 / (N * H * W))  # C, 2, 2
+            w_mat = compute_whitening_matrix(sigma)  # C, 2, 2
+
             self.running_mean.value += (1 - self.momentum) * (
                 mean - self.running_mean.value
             )
             self.running_var.value += (1 - self.momentum) * (
                 sigma - self.running_var.value
             )
-
         else:
             mean, sigma = self.running_mean.value, self.running_var.value
-            y, *_ = batch_normalize(x, mean, sigma)
-        y = self.weight @ y + self.bias
-        out = (y[:, 0, :] + 1j * y[:, 1, :]).reshape(in_shape)
+            w_mat = compute_whitening_matrix(sigma)
+
+        centered = split - mean.reshape(C, 2, 1)
+        y = jn.matmul(w_mat, centered)
+        y = self.weight.value @ y + self.bias.value
+        out = (y[:, 0, :] + 1j * y[:, 1, :]).reshape(N, C, H, W)
         return out
 
     def __repr__(self) -> str:
