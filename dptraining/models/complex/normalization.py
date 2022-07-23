a98a0a4d5990f2f7613601d71f6b3921cb599065
Georgios Kaissis <48105485+gkaissis@users.noreply.github.com>
2022-07-23 12:03:23 +0200

    (finally) fixed complex batch norm

--- dptraining/models/complex/normalization.py
+++ dptraining/models/complex/normalization.py
@@ -5,6 +5,7 @@ from objax.variable import TrainVar
 from objax.typing import JaxArray
 from objax.util import class_name
 from objax.variable import TrainVar, StateVar
+import numpy as onp
 
 
 def gn_single(vec: JaxArray) -> JaxArray:
@@ -78,86 +79,6 @@ class ComplexGroupNorm2DWhitening(ComplexGroupNormWhitening):
 # Batch Norm
 
 
-def _whiten2x2(
-    tensor,
-    training=True,
-    running_mean=None,
-    running_cov=None,
-    momentum=0.1,
-):
-    tail = 1, tensor.shape[2], *([1] * (tensor.ndim - 3))
-    axes = 1, *range(3, tensor.ndim)
-    if training:
-        mean = tensor.mean(axis=axes)
-        if running_mean is not None:
-            running_mean += momentum * (mean - running_mean)
-    else:
-        mean = running_mean
-    tensor = tensor - mean.reshape(2, *tail)
-    if training:
-        var = tensor.var(axis=axes) + 1e-5
-        cov_uu, cov_vv = var[0], var[1]
-        cov_vu = cov_uv = (tensor[0] * tensor[1]).mean([a - 1 for a in axes])
-        if running_cov is not None:
-            cov = jn.stack(
-                [
-                    cov_uu,
-                    cov_uv,
-                    cov_vu,
-                    cov_vv,
-                ],
-                axis=0,
-            ).reshape(2, 2, -1)
-            running_cov += momentum * (cov - running_cov)
-    else:
-        cov_uu, cov_uv = running_cov[0, 0], running_cov[0, 1]
-        cov_vu, cov_vv = running_cov[1, 0], running_cov[1, 1]
-    sqrdet = jn.sqrt(cov_uu * cov_vv - cov_uv * cov_vu)
-    denom = sqrdet * jn.sqrt(cov_uu + 2 * sqrdet + cov_vv)
-    p, q = (cov_vv + sqrdet) / denom, -cov_uv / denom
-    r, s = -cov_vu / denom, (cov_uu + sqrdet) / denom
-    out = jn.stack(
-        [
-            tensor[0] * p.reshape(tail) + tensor[1] * r.reshape(tail),
-            tensor[0] * q.reshape(tail) + tensor[1] * s.reshape(tail),
-        ],
-        axis=0,
-    )
-    return out
-
-
-def batch_norm(
-    input,
-    running_mean,
-    running_var,
-    weight=None,
-    bias=None,
-    training=False,
-    momentum=0.1,
-):
-    x = jn.stack([input.real, input.imag], axis=0)
-    z = _whiten2x2(
-        x,
-        training=training,
-        running_mean=running_mean,
-        running_cov=running_var,
-        momentum=momentum,
-    )
-
-    if weight is not None and bias is not None:
-        shape = 1, x.shape[2], *([1] * (x.ndim - 3))
-        weight = weight.reshape(2, 2, *shape)
-        z = jn.stack(
-            [
-                z[0] * weight[0, 0] + z[1] * weight[0, 1],
-                z[0] * weight[1, 0] + z[1] * weight[1, 1],
-            ],
-            axis=0,
-        ) + bias.reshape(2, *shape)
-
-    return z[0] + 1j * z[1]
-
-
 class ComplexBatchNorm2D(Module):
     """Applies a batch normalization on different ranks of an input tensor.
     The module follows the operation described in Algorithm 1 of
@@ -166,7 +87,7 @@ class ComplexBatchNorm2D(Module):
     https://github.com/ivannz/cplxmodule/blob/master/cplxmodule/nn/modules/batchnorm.py
     """
 
-    def __init__(self, nin: int, momentum: float = 0.1, eps: float = 1e-6):
+    def __init__(self, nin: int, momentum: float = 0.999, eps: float = 1e-6):
         """Creates a BatchNorm2D module instance.
         Args:
             nin: number of channels in the input example.
@@ -179,7 +100,10 @@ class ComplexBatchNorm2D(Module):
         self.momentum = momentum
 
         self.running_mean = StateVar(jn.zeros((2, nin)))
-        self.running_var = StateVar(jn.tile(jn.eye(2), (nin, 1, 1)).reshape(2, 2, nin))
+        running_var = onp.zeros((2, 2, nin))
+        running_var[0, 0] = 1.0
+        running_var[1, 1] = 1.0
+        self.running_var = StateVar(jn.array(running_var))
 
         self.bias = TrainVar(jn.zeros((nin, 2, 1)))  # (n_channels, 2, 1)
         gamma_rr = gamma_ii = functional.rsqrt(2.0)
@@ -188,27 +112,68 @@ class ComplexBatchNorm2D(Module):
         weight = jn.tile(weight, (self.nin, 1, 1))
         self.weight = TrainVar(weight)  # (n_channels, 2, 2)
 
-    def __call__(self, x: JaxArray, training: bool | None = True) -> JaxArray:
+    def whiten2x2(self, x, training):
+        tail = 1, x.shape[2], *([1] * (x.ndim - 3))
+        axes = 1, *range(3, x.ndim)
         if training:
-            return batch_norm(
-                x,
-                running_mean=self.running_mean.value,
-                running_var=self.running_var.value,
-                weight=self.weight.value,
-                bias=self.bias.value,
-                training=True,
-                momentum=self.momentum,
-            )
+            mean = x.mean(axis=axes)
+            self.running_mean.value = (self.momentum * mean) + (
+                1 - self.momentum
+            ) * self.running_mean.value
+
         else:
-            return batch_norm(
-                x,
-                running_mean=self.running_mean.value,
-                running_var=self.running_var.value,
-                weight=self.weight.value,
-                bias=self.bias.value,
-                training=True,
-                momentum=self.momentum,
-            )
+            mean = self.running_mean.value
+        x = x - mean.reshape(2, *tail)
+        if training:
+            var = x.var(axis=axes) + 1e-5
+            cov_uu, cov_vv = var[0], var[1]
+            cov_vu = cov_uv = (x[0] * x[1]).mean([a - 1 for a in axes])
+            cov = jn.stack(
+                [
+                    cov_uu,
+                    cov_uv,
+                    cov_vu,
+                    cov_vv,
+                ],
+                axis=0,
+            ).reshape(2, 2, -1)
+            self.running_var.value = (self.momentum * cov) + (
+                1 - self.momentum
+            ) * self.running_var.value
+        else:
+            cov_uu, cov_uv = self.running_var.value[0, 0], self.running_var.value[0, 1]
+            cov_vu, cov_vv = self.running_var.value[1, 0], self.running_var.value[1, 1]
+        sqrdet = jn.sqrt(
+            cov_uu * cov_vv - cov_uv * cov_vu
+        )  # no need to check for negativity since covariance matrix is PSD
+        denom = sqrdet * jn.sqrt(cov_uu + 2 * sqrdet + cov_vv)
+        p, q = (cov_vv + sqrdet) / denom, -cov_uv / denom
+        r, s = -cov_vu / denom, (cov_uu + sqrdet) / denom
+        out = jn.stack(
+            [
+                x[0] * p.reshape(tail) + x[1] * r.reshape(tail),
+                x[0] * q.reshape(tail) + x[1] * s.reshape(tail),
+            ],
+            axis=0,
+        )
+        return out
+
+    def batch_norm(self, x, training):
+        x = jn.stack([x.real, x.imag], axis=0)
+        z = self.whiten2x2(x=x, training=training)
+        shape = 1, x.shape[2], *([1] * (x.ndim - 3))
+        weight = self.weight.value.reshape(2, 2, *shape)
+        z = jn.stack(
+            [
+                z[0] * weight[0, 0] + z[1] * weight[0, 1],
+                z[0] * weight[1, 0] + z[1] * weight[1, 1],
+            ],
+            axis=0,
+        ) + self.bias.value.reshape(2, *shape)
+        return z[0] + 1j * z[1]
+
+    def __call__(self, x: JaxArray, training: bool | None = True) -> JaxArray:
+        return self.batch_norm(x, training=training)
 
     def __repr__(self) -> str:
         return f"{class_name(self)}(nin={self.nin}, momentum={self.momentum})"
