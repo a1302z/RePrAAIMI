cc98884027f44b3abe9e2e04643c64312e950870
Georgios Kaissis <48105485+gkaissis@users.noreply.github.com>
2022-07-24 18:42:54 +0200

    speed up GN

--- dptraining/models/complex/normalization.py
+++ dptraining/models/complex/normalization.py
@@ -1,33 +1,11 @@
 from jax import numpy as jn, vmap
 from objax import functional
 from objax.module import Module
-from objax.variable import TrainVar
 from objax.typing import JaxArray
 from objax.util import class_name
 from objax.variable import TrainVar, StateVar
 import numpy as onp
-
-
-def gn_single(vec: JaxArray) -> JaxArray:
-    """Takes a complex vector with three dimensions and
-    1. computes the mean
-    2. computes the covariance matrix
-    3. centers and whitens the vector
-    This method is optimised for group norm where it is supposed to
-    be vmapped (twice) over the batch and group dimensions. Will give
-    back a split vector ready to apply affine parameters.
-    """
-    split = jn.stack([vec.real, vec.imag]).reshape(2, -1)
-    mean = split.mean(-1, keepdims=True)
-    centered = split - mean
-    sigma = jn.cov(split[0], split[1])
-    u_mat, lmbda, _ = jn.linalg.svd(sigma, full_matrices=False)
-    w_mat = jn.dot(u_mat, jn.dot(jn.diag(1.0 / jn.sqrt(lmbda + 1e-5)), u_mat.T))
-    result = w_mat @ centered
-    return (result[0] + 1j * result[1]).reshape(vec.shape)
-
-
-group_normalize = vmap(vmap(gn_single))
+from typing import Any
 
 
 class ComplexGroupNormWhitening(Module):
@@ -39,7 +17,7 @@ class ComplexGroupNormWhitening(Module):
 class ComplexGroupNorm2DWhitening(ComplexGroupNormWhitening):
     """Applies a 2D group normalization on a input batch of shape (N,C,H,W)."""
 
-    def __init__(self, nin: int, groups: int = 32, eps: float = 1e-5) -> None:
+    def __init__(self, nin: int, groups: int = 32, eps: Any = 1e-5) -> None:
         """Creates a GroupNorm2D module instance.
         Args:
             nin: number of input channels.
@@ -52,31 +30,55 @@ class ComplexGroupNorm2DWhitening(ComplexGroupNormWhitening):
         del eps
         self.nin = nin
         self.groups = groups
-        self.bias = TrainVar(jn.zeros((1, nin, 2, 1)))  # (1, C, 2 ,1)
-        gamma_rr = gamma_ii = functional.rsqrt(2.0)
-        gamma_ri = 0.0
-        weight = jn.array([[gamma_rr, gamma_ri], [gamma_ri, gamma_ii]])
-        weight = jn.tile(weight, (1, self.nin, 1, 1))
-        self.weight = TrainVar(weight)  # (1, C, 2, 2)
+        self.bias = TrainVar(
+            jn.zeros((1, nin, 2, 1)).reshape(2, 1, nin, 1, 1)
+        )  # (2, 1, C, 1 ,1)
+        weight = onp.zeros((2, 2, 1, nin, 1, 1))  # (2, 2, 1, C, 1, 1)
+        weight[0, 0] = weight[1, 1] = functional.rsqrt(2.0)
+        weight[1, 0] = weight[0, 1] = 0.0
+        self.weight = TrainVar(weight)
 
-    def __call__(self, x: JaxArray, training: bool | None = True) -> JaxArray:
+    def __call__(self, x: JaxArray, training: Any) -> JaxArray:
         del training
         N, C, H, W = x.shape
         G = self.groups
-        x = x.reshape(N, G, C // G, H, W)
-        y = group_normalize(x)
-        z = y.reshape(N, C, H, W)
-        u = jn.stack((z.real, z.imag), 2).reshape(N, C, 2, -1)  # N, C, 2, ...
-        # (1, C, 2, 2) @ (N, C, 2, ...) -> (N, C, 2, ...)
-        # (N, C, 2, ...) + (1, C, 2, 1) -> (N, C, 2, ...)
-        r = jn.matmul(self.weight.value, u) + self.bias.value
-        return (r[:, :, 0, :] + 1j * r[:, :, 1, :]).reshape(N, C, H, W)
+        x = x.reshape(N, G, C // G, H, W)  # N, G, C', H, W
+        x = jn.stack((x.real, x.imag), axis=0)  # 2, N, G, C', H, W
+        mean = x.mean(axis=(3, 4, 5), keepdims=True)  # 2, N, G, 1, 1, 1
+        x = x - mean  # 2, N, G, C', H, W
+        var = x.var(axis=(3, 4, 5)) + 1e-5  # 2, N, G
+        cov_uu, cov_vv = var[0], var[1]  # N, G
+        cov_vu = cov_uv = (x[0] * x[1]).mean(axis=(2, 3, 4))  # N,G
+        sqrdet = jn.sqrt(cov_uu * cov_vv - cov_uv * cov_vu)
+        denom = sqrdet * jn.sqrt(cov_uu + 2 * sqrdet + cov_vv)
+        p, q = (cov_vv + sqrdet) / denom, -cov_uv / denom
+        r, s = -cov_vu / denom, (cov_uu + sqrdet) / denom
+        tail = N, G, 1, 1, 1
+        z = jn.stack(
+            [
+                x[0] * p.reshape(tail) + x[1] * r.reshape(tail),
+                x[0] * q.reshape(tail) + x[1] * s.reshape(tail),
+            ],
+            axis=0,
+        )  # 2, N, G, C', H, W
+        z = z.reshape(2, N, C, H, W)  # 2, N, C, H, W
+        weight = self.weight.value  # 2, 2, 1, C, 1, 1
+        z = (
+            jn.stack(
+                [
+                    z[0] * weight[0, 0] + z[1] * weight[0, 1],
+                    z[0] * weight[1, 0] + z[1] * weight[1, 1],
+                ],
+                axis=0,
+            )
+            + self.bias.value
+        )  # 2, 1, C, 1, 1
+        return z[0] + 1j * z[1]
 
     def __repr__(self) -> str:
         return f"{class_name(self)}(nin={self.nin}, groups={self.groups})"
 
 
-# Batch Norm
 class ComplexBatchNorm2D(Module):
     """Applies a batch normalization on different ranks of an input tensor.
     The module follows the operation described in Algorithm 1 of
@@ -85,7 +87,7 @@ class ComplexBatchNorm2D(Module):
     https://github.com/ivannz/cplxmodule/blob/master/cplxmodule/nn/modules/batchnorm.py
     """
 
-    def __init__(self, nin: int, momentum: float = 0.999, eps: float = 1e-6):
+    def __init__(self, nin: int, momentum: float = 0.999, eps: Any = 1e-6):
         """Creates a BatchNorm2D module instance.
         Args:
             nin: number of channels in the input example.
@@ -109,7 +111,7 @@ class ComplexBatchNorm2D(Module):
         weight = jn.tile(weight, (self.nin, 1, 1))
         self.weight = TrainVar(weight)  # (n_channels, 2, 2)
 
-    def whiten2x2(self, x, training):
+    def whiten2x2(self, x: JaxArray, training: bool | None) -> JaxArray:
         tail = 1, x.shape[2], *([1] * (x.ndim - 3))
         axes = 1, *range(3, x.ndim)
         if training:
@@ -155,7 +157,7 @@ class ComplexBatchNorm2D(Module):
         )
         return out
 
-    def batch_norm(self, x, training):
+    def batch_norm(self, x: JaxArray, training: bool | None) -> JaxArray:
         x = jn.stack([x.real, x.imag], axis=0)
         z = self.whiten2x2(x=x, training=training)
         shape = 1, x.shape[2], *([1] * (x.ndim - 3))
