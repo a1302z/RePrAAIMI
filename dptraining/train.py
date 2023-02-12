4da6912be660379583a4251a5f8aab9e19475fa1
Debargho Basak <54078682+Debargho99@users.noreply.github.com>
2023-02-12 16:18:55 +0100

    Update train.py

--- dptraining/train.py
+++ dptraining/train.py
@@ -13,8 +13,8 @@ from tqdm import tqdm
 sys.path.insert(0, str(Path.cwd()))
 
 
-#os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 # os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
 
 
@@ -31,7 +31,7 @@ def main(
     if isinstance(config, omegaconf.dictconfig.DictConfig):
         config = OmegaConf.to_container(config)
     if "cpu" in config["general"] and config["general"]["cpu"]:
-        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+        os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
     # This is absolutely disgusting but necessary to disable gpu training
     import objax
